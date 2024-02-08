03da3899589629e7c7e34864303d2d7955b2ad51
Juliano Lorenzato <julianolorenzato@gmail.com>
2024-02-07 21:33:52 -0300

    new name: emuk

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,10 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "emuk",
+	Short: "Emuk is a CPU and memory simulator",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Here")
+	},
+}
 
 func Execute() {
-	rootCmd = &cobra.Command{
-		Use: "csimu",
-		Short: "",
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
